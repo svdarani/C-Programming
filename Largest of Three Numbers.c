9ebf7713b5210728ff388d30c9be23c3993a97de
svdarani <svdarani21@gmail.com>
2026-02-12 11:47:36 +0530

    Add files via upload

---
+++ Largest of Three Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d", &a,&b,&c);
+    
+    if (a>b && a>c) {
+        printf("%d is largest",a);
+    }
+    else if (b>c && b>a) {
+        printf("%d is largest",b);
+    }
+    else {
+        printf("%d is largest",c);
+    }
+    return 0;
+}
